65ea4d996b3700ed88a97feccbaff53d3e528c83
Shreya Krishna Singh <127198185+Shreyas25869@users.noreply.github.com>
2026-09-07 11:39:38 +0530

    Update config.js

--- person2/js/config.js
+++ person2/js/config.js
@@ -5,5 +5,5 @@
 */
 window.APP_CONFIG = {
   SUPABASE_URL: "https://ntkbegbletsjexwcbmob.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50a2JlZ2JsZXRzamV4d2NibW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3MTk4MDEsImV4cCI6MjEwNDI5NTgwMX0.X7N67d1B_TP4lNllfEeQT1HSUbkIJLPc3dDHSJw9irs"
+  SUPABASE_ANON_KEY: "sb_publishable_EDcKx8xT5jLwsYa9PTJF6w_N4kcMWSN"
 };
